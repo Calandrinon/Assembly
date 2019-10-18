343234318030e79b0d28887ca174404dcbbd758a
George <georgesut@yahoo.com>
2019-10-18 21:56:53 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tools for Computer System Architecture lab
+Some tools for the computer system architecture lab and a bash script written by me to automate the assembling process for Linux with wine.
---
+++ npp/run_script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+project_name = $1
+echo "$1"
+#asm_filename = "${project_name}.asm"
+#obj_filename = "${project_name}.obj"
+#exe_filename = "${project_name}.exe"
+#wine ../nasm/nasm.exe -fobj asm_filename
+#wine ../nasm/ALINK.EXE -oPE -subsys console -entry start obj_filename
+#wine ../ollydbg/ollydbg.exe exe_filename
